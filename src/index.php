e0f1fc64c244c82007b5584d9bfac3d136632714
eisidis <julien023001@gmail.com>
2024-07-15 15:39:13 +0200

    Initialisation du projet

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Docker/PHP</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <script src="./assets/js/script.js"></script>
+</body>
+</html>
